8c09784b47ea73d2cd0f545ebf49439685f044f9
Loic Nicolas <loicnicolas06@hotmail.com>
2018-04-13 15:35:59 +0200

    modifs mineures

--- src/main/java/com/formation/tpbootstrap/controller/IndexController.java
+++ src/main/java/com/formation/tpbootstrap/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.formation.tpbootstrap.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -7,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/")
@@ -19,7 +24,7 @@ public class IndexController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String indexPost(final ModelMap pModel, HttpServletRequest request) {
+	public String indexPost(final ModelMap pModel, HttpServletRequest request, @RequestParam("file") MultipartFile file) {
 		
 		HttpSession session = request.getSession();
 		
